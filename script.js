a3818a34d970a19f33669b0da2fbe84f01d5d6ca
Bongeka <bongekathabede05@gmail.com>
2025-06-08 11:18:54 +0200

    i commit to all the changes i've made

---
+++ script.js
@@ -0,0 +1,21 @@
+function append(character) {
+    document.getElementById('display').value +=character;
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value =eval(document.getElementById('display').value);
+    }  catch (e) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value ='';
+}
+
+function playSound() {
+    const sound =document.getElementById('clickSound');
+    sound.currentTime = 0;
+    sound.play();
+}
